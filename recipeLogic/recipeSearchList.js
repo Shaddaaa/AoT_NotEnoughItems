3c66ee3f1c4db4e9e424846b71a61ebbdd57b397
Shaddaaa <florian@kaufmann.cloud>
2020-08-25 22:44:22 +0200

    updated the looks of displayed recipes

--- recipeLogic/recipeSearchList.js
+++ recipeLogic/recipeSearchList.js
@@ -32,22 +32,116 @@ function displayRecipe(recipeID) {
         display.innerHTML = "";
         activeRecipeID = recipeID;
 
-        //display recipe
-        display.innerHTML += "Ingredients: ";
+
+        let table = document.createElement("table");
+        display.appendChild(table);
+
+        let headerRow = document.createElement("tr");
+        let headers = ["Ingredients:", "Tools:", "", "Results:"];
+        for (let headerText of headers) {
+            let header = document.createElement("th");
+            header.innerHTML = headerText;
+            headerRow.appendChild(header);
+        }
+        table.appendChild(headerRow);
+        
+        let max = Math.max(recipe.ingredientStacks.length-1, recipe.resultStacks.length, recipe.toolStacks.length, 2);
+        for (let i = 0; i < max; i++) {
+            let ingredientStack = recipe.ingredientStacks[i];
+            let toolStack = recipe.toolStacks[i];
+            let resultStack = recipe.resultStacks[i];
+            console.log(i);
+            let tr = document.createElement("tr");
+            let td1 = document.createElement("td");
+            let td2 = document.createElement("td");
+            let td3 = document.createElement("td");
+            let td4 = document.createElement("td");
+
+            //td1
+            if (ingredientStack!=null && ingredientStack.item.name != "time") {
+                td1.innerHTML += ingredientStack.size.toFixed(2) + " " + upEveryFirstLetter(ingredientStack.item.name);
+            }
+            //td2
+            if (toolStack!=null) {
+                td2.innerHTML += toolStack.size.toFixed(2) + " " + upEveryFirstLetter(toolStack.item.name);
+            }
+            //td3
+            if (i == Math.floor(max/2)-1) {
+                let arrow = document.createElement("div");
+                arrow.classList.toggle("arrowRight");
+                td3.appendChild(arrow);
+            }
+            if (i == Math.floor(max/2)) {
+                let timeIndex = recipe.getIngredientIndexOf("time");
+                let time;
+                if (timeIndex!=-1) {
+                    time = recipe.ingredientStacks[timeIndex].size;
+                } else {
+                    time = 0;
+                }
+                td3.innerHTML = time + "🕑";
+            }
+            //td4
+            if (resultStack!=null) {
+                td4.innerHTML += resultStack.size.toFixed(2) + " " + upEveryFirstLetter(resultStack.item.name);
+            }
+            tr.appendChild(td1);
+            tr.appendChild(td2);
+            tr.appendChild(td3);
+            tr.appendChild(td4);
+            table.appendChild(tr);
+        }
+
+        /*let ingredients = document.createElement("div");
+        ingredients.id = "recipeIngredientColumn";
+        ingredients.classList.toggle("recipeDisplayColumn");
+        ingredients.appendChild(iHeader);
         for (let ingredientStack of recipe.ingredientStacks) {
-            display.innerHTML += ingredientStack.size.toFixed(2) + "x " + ingredientStack.item.name + ", ";
+            let ingredient = document.createElement("div");
+            ingredient.classList.toggle("recipeTableElement");
+            ingredient.innerHTML += ingredientStack.size.toFixed(2) + "x " + ingredientStack.item.name;
+            ingredients.appendChild(ingredient);
         }
+        table.appendChild(ingredients);
 
-        display.innerHTML += "<br>Tools: ";
+
+
+        let tools = document.createElement("div");
+        tools.id = "recipeToolColumn";
+        tools.classList.toggle("recipeDisplayColumn");
+        tools.appendChild(tHeader);
         for (let toolStack of recipe.toolStacks) {
-            display.innerHTML += toolStack.size.toFixed(2) + "x " + toolStack.item.name + ", ";
+            let tool = document.createElement("div");
+            tool.classList.toggle("recipeTableElement");
+            tool.innerHTML += toolStack.size.toFixed(2) + "x " + toolStack.item.name;
+            tools.appendChild(tool);
         }
+        table.appendChild(tools);
 
-        display.innerHTML += "<br>Results: ";
+        let arrowContainer = document.createElement("div");
+        arrowContainer.id = "recipeArrowColumn";
+        arrowContainer.classList.toggle("recipeDisplayColumn");
+        let arrowDiv = document.createElement("div");
+        let arrow = document.createElement("div");
+        arrow.classList.toggle("arrowRight");
+        arrowDiv.append(arrow);
+        arrowContainer.appendChild(arrowDiv);
+        table.appendChild(arrowContainer);
+
+
+        let results = document.createElement("div");
+        results.id = "recipeResultColumn";
+        results.classList.toggle("recipeDisplayColumn");
+        results.appendChild(rHeader);
         for (let resultStack of recipe.resultStacks) {
-            display.innerHTML += resultStack.size.toFixed(2) + "x " + resultStack.item.name + ", ";
+            let result = document.createElement("div");
+            result.classList.toggle("recipeTableElement");
+            result.innerHTML += resultStack.size.toFixed(2) + "x " + resultStack.item.name;
+            results.appendChild(result);
         }
+        table.appendChild(results);
 
+        */
         //display recipe options
         let successInput = document.getElementById("recipeSuccessChance").children[1];
         successInput.placeholder = recipe.successChance;
